584d3aad603ef2769abd71fbe3cb3dc0aba5cf36
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2022-03-15 22:02:58 +0100

    mahdi soultana solution

---
+++ src/toast/Toast.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+export const ToastContainer = ({ className = "", children, ...otherProps }) => {
+  return (
+    <aside
+      className={`fixed h-auto overflow-y-auto top-4 right-[50%]  translate-x-[50%] lg:-translate-x-[10%] lg:top-auto lg:bottom-4 max-h-[100vh] lg:right-4 bg-black    text-center ${className}`}
+      {...otherProps}
+    >
+      {children}
+    </aside>
+  );
+};
+
+//this toat component is configurable by the time and type
+function Toast({
+  timer = 3,
+  type = "success",
+  typeColor = {
+    success: "bg-green-500",
+    warning: "bg-orange-500",
+    error: "bg-red-500",
+  },
+  clearToast,
+  className = "",
+  id,
+  children,
+  ...otherProps
+}) {
+  const [show, setShow] = useState(false);
+
+  React.useEffect(() => {
+    setShow(true);
+    let timerFn = setTimeout(() => {
+      setShow(false);
+      clearToast(id);
+    }, timer);
+    return () => clearTimeout(timerFn);
+  }, [clearToast, id, timer]);
+  return (
+    <>
+      {show ? (
+        <div
+          className={`${typeColor[type]} relative m-3 p-3 px-4 font-medium rounded-md shadow-md ${className}`}
+          {...otherProps}
+        >
+          <button
+            className="hover:bg-red-700 w-4 h-4 flex items-center justify-center text-white  rounded-sm absolute top-1 right-1"
+            onClick={() => {
+              clearToast(id);
+            }}
+          >
+            &times;
+          </button>
+          {children}
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+export default Toast;
--- src/toast/ToastProvider.tsx
+++ src/toast/ToastProvider.tsx
@@ -1,23 +1,83 @@
+import React, { useState } from "react";
 import { ToastContext } from "./ToastContext";
-
+import Toast, { ToastContainer } from "./Toast";
 type Props = {
   children: React.ReactNode;
 };
 
-// your mission: implement this component
 export function ToastProvider(props: Props) {
   const { children } = props;
+  /////////
+  const [toasts, setToasts] = useState([]);
 
-  return (
-    <ToastContext.Provider
-      value={{
-        stats: null,
-        addToast: () => {
-          throw new Error("not implemented");
+  const [stats, setState] = React.useState({
+    success: 0,
+    warning: 0,
+    error: 0,
+  });
+
+  function addToast(params) {
+    let {
+      type = "success",
+      ttl,
+      body = "you are doing good so far 🥰",
+    } = params;
+    ttl = ttl == null ? 500 : ttl;
+    console.log(ttl);
+
+    setToasts((prevS) => {
+      return [
+        ...prevS,
+        {
+          key: `toast-${Math.random() * 100}`,
+          type,
+          ttl,
+          body,
         },
-      }}
-    >
+      ];
+    });
+
+    fetch(`/api/toasts/${type}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setState(data);
+      });
+  }
+
+  // console.log(toasts);
+  //this function is just to clear the teast from the state
+
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Memory_Management#garbage_collection
+
+  function clearToast(id) {
+    setToasts((prevS) => {
+      return prevS.filter((toast) => id !== toast.key);
+    });
+  }
+  /////////////
+  return (
+    <ToastContext.Provider value={{ stats, addToast }}>
       {children}
+
+      <ToastContainer>
+        {toasts.map((toast) => (
+          <Toast
+            key={toast.key}
+            id={toast.key}
+            type={toast.type}
+            timer={Number(toast.ttl)}
+            clearToast={clearToast}
+          >
+            {toast.body || "Helloo i'm fallback"}
+          </Toast>
+        ))}
+      </ToastContainer>
     </ToastContext.Provider>
   );
 }
